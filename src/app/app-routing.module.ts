a899dc217d7a97ea77e8d7029364ab454282042f
emir57 <emir.gurbuz06@hotmail.com>
2021-12-04 21:12:16 +0300

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
+import { LoginGuard } from './guard/login.guard';
 
 const routes: Routes = [
   {path:"",pathMatch:'full',component:ProductComponent},
   {path:"products",component:ProductComponent},
-  {path:"products/add",component:ProductAddComponent},
+  {path:"products/add",component:ProductAddComponent,canActivate:[LoginGuard]},
   {path:"products/category/:categoryId",component:ProductComponent},
   {path:"login",component:LoginComponent},
 ];
